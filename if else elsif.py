98599fc3b83a882e86173e1d35c4661beaa69a91
Asadraza2540 <asadraza2540@gmail.com>
2025-09-22 17:54:33 +0500

    Create if else elsif.py

---
+++ if else elsif.py
@@ -0,0 +1,28 @@
+#
+
+student_age= 22
+student_height=3.4
+student_name= 'asad'
+print("student age is =:",student_age)
+print("student height is=:",student_height)
+print("student name is =:",student_name)
+print(type(student_name))
+print(type(student_height))
+print(type(student_name))
+
+sum= student_age+student_height
+print(sum)
+print(type(sum))
+
+substract=student_age-student_height
+print("substracted value is equal to=:",substract)
+print(type(substract))
+
+if student_age > student_height:
+    print("age is greater :")
+elif student_age <student_height:
+  print('age is less:')  
+else:
+   print("both are equal")
+
+   
