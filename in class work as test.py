523b74089c45a0af3e61f8ee1226229a226c5420
Asadraza2540 <asadraza2540@gmail.com>
2025-09-22 17:23:47 +0500

    Create in class work as test.py

---
+++ in class work as test.py
@@ -0,0 +1,12 @@
+#
+
+student_age= 22
+student_height=3.4
+student_name= 'asad'
+print("student age is =:",student_age)
+print("student height is=:",student_height)
+print("student name is =:",student_name)
+print(type(student_name))
+print(type(student_height))
+print(type(student_name))
+
